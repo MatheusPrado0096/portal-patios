d32972a6587676333eaf281453aa16150928338a
MatheusPrado0096 <matheusdprado179@gmail.com>
2026-09-16 10:59:46 -0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,574 @@
+window.PATIOS = [
+  {
+    "id": "FCA",
+    "nome": "Caçapava",
+    "categoria": "ABRIGO",
+    "label": "Abrigos",
+    "planta": "docs/plantas/FCA.pdf",
+    "itens": [
+      {
+        "tipo": "Abrigo",
+        "nome": "ABR05",
+        "codigo": "FCA01",
+        "km": "",
+        "pdf": "docs/equipamentos/FCA01.pdf",
+        "link": ""
+      },
+      {
+        "tipo": "Abrigo",
+        "nome": "ABR10",
+        "codigo": "FCA02",
+        "km": "",
+        "pdf": "docs/equipamentos/FCA02.pdf",
+        "link": ""
+      },
+      {
+        "tipo": "Abrigo",
+        "nome": "ABR15",
+        "codigo": "FCA03",
+        "km": "",
+        "pdf": "docs/equipamentos/FCA03.pdf",
+        "link": ""
+      },
+      {
+        "tipo": "Abrigo",
+        "nome": "ABR20",
+        "codigo": "FCA04",
+        "km": "",
+        "pdf": "docs/equipamentos/FCA04.pdf",
+        "link": ""
+      },
+      {
+        "tipo": "Abrigo",
+        "nome": "ABR25",
+        "codigo": "FCA05",
+        "km": "",
+        "pdf": "docs/equipamentos/FCA05.pdf",
+        "link": ""
+      },
+      {
+        "tipo": "Abrigo",
+        "nome": "ABR30",
+        "codigo": "FCA06",
+        "km": "",
+        "pdf": "docs/equipamentos/FCA06.pdf",
+        "link": ""
+      }
+    ]
+  },
+  {
+    "id": "FEA",
+    "nome": "Edemar Alves",
+    "categoria": "ABRIGO",
+    "label": "Abrigos",
+    "planta": "docs/plantas/FEA.pdf",
+    "itens": [
+      {
+        "tipo": "Abrigo",
+        "nome": "ABR05",
+        "codigo": "FEA01",
+        "km": "",
+        "pdf": "docs/equipamentos/FEA01.pdf",
+        "link": ""
+      },
+      {
+        "tipo": "Abrigo",
+        "nome": "ABR10",
+        "codigo": "FEA02",
+        "km": "",
+        "pdf": "docs/equipamentos/FEA02.pdf",
+        "link": ""
+      },
+      {
+        "tipo": "Abrigo",
+        "nome": "ABR15",
+        "codigo": "FEA03",
+        "km": "",
+        "pdf": "docs/equipamentos/FEA03.pdf",
+        "link": ""
+      },
+      {
+        "tipo": "Abrigo",
+        "nome": "ABR20",
+        "codigo": "FEA04",
+        "km": "",
+        "pdf": "docs/equipamentos/FEA04.pdf",
+        "link": ""
+      }
+    ]
+  },
+  {
+    "id": "FEM",
+    "nome": "Martins Guimarães",
+    "categoria": "ABRIGO",
+    "label": "Abrigos",
+    "planta": "docs/plantas/FEM.pdf",
+    "itens": [
+      {
+        "tipo": "Abrigo",
+        "nome": "ABR05",
+        "codigo": "FEM01",
+        "km": "",
+        "pdf": "docs/equipamentos/FEM01.pdf",
+        "link": ""
+      },
+      {
+        "tipo": "Abrigo",
+        "nome": "ABR10",
+        "codigo": "FEM02",
+        "km": "",
+        "pdf": "docs/equipamentos/FEM02.pdf",
+        "link": ""
+      },
+      {
+        "tipo": "Abrigo",
+        "nome": "ABR15",
+        "codigo": "FEM03",
+        "km": "",
+        "pdf": "docs/equipamentos/FEM03.pdf",
+        "link": ""
+      }
+    ]
+  },
+  {
+    "id": "FSJ",
+    "nome": "São José dos Campos",
+    "categoria": "ABRIGO",
+    "label": "Abrigos",
+    "planta": "docs/plantas/FSJ.pdf",
+    "itens": [
+      {
+        "tipo": "Abrigo",
+        "nome": "ABR05",
+        "codigo": "FSJ01",
+        "km": "",
+        "pdf": "docs/equipamentos/FSJ01.pdf",
+        "link": ""
+      },
+      {
+        "tipo": "Abrigo",
+        "nome": "ABR10",
+        "codigo": "FSJ02",
+        "km": "",
+        "pdf": "docs/equipamentos/FSJ02.pdf",
+        "link": ""
+      },
+      {
+        "tipo": "Abrigo",
+        "nome": "ABR15",
+        "codigo": "FSJ03",
+        "km": "",
+        "pdf": "docs/equipamentos/FSJ03.pdf",
+        "link": ""
+      },
+      {
+        "tipo": "Abrigo",
+        "nome": "ABR20",
+        "codigo": "FSJ04",
+        "km": "",
+        "pdf": "docs/equipamentos/FSJ04.pdf",
+        "link": ""
+      },
+      {
+        "tipo": "Abrigo",
+        "nome": "ABR25",
+        "codigo": "FSJ05",
+        "km": "",
+        "pdf": "docs/equipamentos/FSJ05.pdf",
+        "link": ""
+      },
+      {
+        "tipo": "Abrigo",
+        "nome": "ABR30",
+        "codigo": "FSJ06",
+        "km": "",
+        "pdf": "docs/equipamentos/FSJ06.pdf",
+        "link": ""
+      },
+      {
+        "tipo": "Abrigo",
+        "nome": "ABR35",
+        "codigo": "FSJ07",
+        "km": "",
+        "pdf": "docs/equipamentos/FSJ07.pdf",
+        "link": ""
+      },
+      {
+        "tipo": "Abrigo",
+        "nome": "ABR40",
+        "codigo": "FSJ08",
+        "km": "",
+        "pdf": "docs/equipamentos/FSJ08.pdf",
+        "link": ""
+      },
+      {
+        "tipo": "Abrigo",
+        "nome": "ABR45",
+        "codigo": "FSJ09",
+        "km": "",
+        "pdf": "docs/equipamentos/FSJ09.pdf",
+        "link": ""
+      }
+    ]
+  },
+  {
+    "id": "FPA",
+    "nome": "Pagador Andrade",
+    "categoria": "ABRIGO",
+    "label": "Abrigos",
+    "planta": "docs/plantas/FPA.pdf",
+    "itens": [
+      {
+        "tipo": "Abrigo",
+        "nome": "ABR05",
+        "codigo": "FPA01",
+        "km": "",
+        "pdf": "docs/equipamentos/FPA01.pdf",
+        "link": ""
+      },
+      {
+        "tipo": "Abrigo",
+        "nome": "ABR10",
+        "codigo": "FPA02",
+        "km": "",
+        "pdf": "docs/equipamentos/FPA02.pdf",
+        "link": ""
+      },
+      {
+        "tipo": "Abrigo",
+        "nome": "ABR15",
+        "codigo": "FPA03",
+        "km": "",
+        "pdf": "docs/equipamentos/FPA03.pdf",
+        "link": ""
+      },
+      {
+        "tipo": "Abrigo",
+        "nome": "ABR20",
+        "codigo": "FPA04",
+        "km": "",
+        "pdf": "docs/equipamentos/FPA04.pdf",
+        "link": ""
+      },
+      {
+        "tipo": "Abrigo",
+        "nome": "ABR25",
+        "codigo": "FPA05",
+        "km": "",
+        "pdf": "docs/equipamentos/FPA05.pdf",
+        "link": ""
+      },
+      {
+        "tipo": "Abrigo",
+        "nome": "ABR30",
+        "codigo": "FPA06",
+        "km": "",
+        "pdf": "docs/equipamentos/FPA06.pdf",
+        "link": ""
+      }
+    ]
+  },
+  {
+    "id": "FRE",
+    "nome": "Remédios",
+    "categoria": "ABRIGO",
+    "label": "Abrigos",
+    "planta": "docs/plantas/FRE.pdf",
+    "itens": [
+      {
+        "tipo": "Abrigo",
+        "nome": "ABR05",
+        "codigo": "FRE01",
+        "km": "",
+        "pdf": "docs/equipamentos/FRE01.pdf",
+        "link": ""
+      },
+      {
+        "tipo": "Abrigo",
+        "nome": "ABR10",
+        "codigo": "FRE02",
+        "km": "",
+        "pdf": "docs/equipamentos/FRE02.pdf",
+        "link": ""
+      },
+      {
+        "tipo": "Abrigo",
+        "nome": "ABR15",
+        "codigo": "FRE03",
+        "km": "",
+        "pdf": "docs/equipamentos/FRE03.pdf",
+        "link": ""
+      },
+      {
+        "tipo": "Abrigo",
+        "nome": "ABR20",
+        "codigo": "FRE04",
+        "km": "",
+        "pdf": "docs/equipamentos/FRE04.pdf",
+        "link": ""
+      },
+      {
+        "tipo": "Abrigo",
+        "nome": "ABR25",
+        "codigo": "FRE05",
+        "km": "",
+        "pdf": "docs/equipamentos/FRE05.pdf",
+        "link": ""
+      },
+      {
+        "tipo": "Abrigo",
+        "nome": "ABR30",
+        "codigo": "FRE06",
+        "km": "",
+        "pdf": "docs/equipamentos/FRE06.pdf",
+        "link": ""
+      }
+    ]
+  },
+  {
+    "id": "FIP",
+    "nome": "Itapeti",
+    "categoria": "ABRIGO",
+    "label": "Abrigos",
+    "planta": "docs/plantas/FIP.pdf",
+    "itens": [
+      {
+        "tipo": "Abrigo",
+        "nome": "ABR05",
+        "codigo": "FIP01",
+        "km": "",
+        "pdf": "docs/equipamentos/FIP01.pdf",
+        "link": ""
+      },
+      {
+        "tipo": "Abrigo",
+        "nome": "ABR10",
+        "codigo": "FIP02",
+        "km": "",
+        "pdf": "docs/equipamentos/FIP02.pdf",
+        "link": ""
+      },
+      {
+        "tipo": "Abrigo",
+        "nome": "ABR15",
+        "codigo": "FIP03",
+        "km": "",
+        "pdf": "docs/equipamentos/FIP03.pdf",
+        "link": ""
+      },
+      {
+        "tipo": "Abrigo",
+        "nome": "ABR20",
+        "codigo": "FIP04",
+        "km": "",
+        "pdf": "docs/equipamentos/FIP04.pdf",
+        "link": ""
+      }
+    ]
+  },
+  {
+    "id": "FST",
+    "nome": "São Bento",
+    "categoria": "ABRIGO",
+    "label": "Abrigos",
+    "planta": "docs/plantas/FST.pdf",
+    "itens": [
+      {
+        "tipo": "Abrigo",
+        "nome": "ABR05",
+        "codigo": "FST01",
+        "km": "",
+        "pdf": "docs/equipamentos/FST01.pdf",
+        "link": ""
+      },
+      {
+        "tipo": "Abrigo",
+        "nome": "ABR10",
+        "codigo": "FST02",
+        "km": "",
+        "pdf": "docs/equipamentos/FST02.pdf",
+        "link": ""
+      },
+      {
+        "tipo": "Abrigo",
+        "nome": "ABR15",
+        "codigo": "FST03",
+        "km": "",
+        "pdf": "docs/equipamentos/FST03.pdf",
+        "link": ""
+      },
+      {
+        "tipo": "Abrigo",
+        "nome": "ABR20",
+        "codigo": "FST04",
+        "km": "",
+        "pdf": "docs/equipamentos/FST04.pdf",
+        "link": ""
+      },
+      {
+        "tipo": "Abrigo",
+        "nome": "ABR25",
+        "codigo": "FST05",
+        "km": "",
+        "pdf": "docs/equipamentos/FST05.pdf",
+        "link": ""
+      },
+      {
+        "tipo": "Abrigo",
+        "nome": "ABR30",
+        "codigo": "FST06",
+        "km": "",
+        "pdf": "docs/equipamentos/FST06.pdf",
+        "link": ""
+      },
+      {
+        "tipo": "Abrigo",
+        "nome": "ABR35",
+        "codigo": "FST07",
+        "km": "",
+        "pdf": "docs/equipamentos/FST07.pdf",
+        "link": ""
+      },
+      {
+        "tipo": "Abrigo",
+        "nome": "ABR40",
+        "codigo": "FST08",
+        "km": "",
+        "pdf": "docs/equipamentos/FST08.pdf",
+        "link": ""
+      },
+      {
+        "tipo": "Abrigo",
+        "nome": "ABR45",
+        "codigo": "FST09",
+        "km": "",
+        "pdf": "docs/equipamentos/FST09.pdf",
+        "link": ""
+      }
+    ]
+  },
+  {
+    "id": "FPI",
+    "nome": "Pinheirinho",
+    "categoria": "ABRIGO",
+    "label": "Abrigos",
+    "planta": "docs/plantas/FPI.pdf",
+    "itens": [
+      {
+        "tipo": "Abrigo",
+        "nome": "ABR05",
+        "codigo": "FPI01",
+        "km": "",
+        "pdf": "docs/equipamentos/FPI01.pdf",
+        "link": ""
+      },
+      {
+        "tipo": "Abrigo",
+        "nome": "ABR10",
+        "codigo": "FPI02",
+        "km": "",
+        "pdf": "docs/equipamentos/FPI02.pdf",
+        "link": ""
+      },
+      {
+        "tipo": "Abrigo",
+        "nome": "ABR15",
+        "codigo": "FPI03",
+        "km": "",
+        "pdf": "docs/equipamentos/FPI03.pdf",
+        "link": ""
+      },
+      {
+        "tipo": "Abrigo",
+        "nome": "ABR20",
+        "codigo": "FPI04",
+        "km": "",
+        "pdf": "docs/equipamentos/FPI04.pdf",
+        "link": ""
+      },
+      {
+        "tipo": "Abrigo",
+        "nome": "ABR25",
+        "codigo": "FPI05",
+        "km": "",
+        "pdf": "docs/equipamentos/FPI05.pdf",
+        "link": ""
+      },
+      {
+        "tipo": "Abrigo",
+        "nome": "ABR30",
+        "codigo": "FPI06",
+        "km": "",
+        "pdf": "docs/equipamentos/FPI06.pdf",
+        "link": ""
+      }
+    ]
+  },
+  {
+    "id": "IEF",
+    "nome": "IEF",
+    "categoria": "ABRIGO",
+    "label": "Abrigos",
+    "planta": "docs/plantas/IEF.pdf",
+    "itens": [
+      {
+        "tipo": "Abrigo",
+        "nome": "ABR05",
+        "codigo": "IEF01",
+        "km": "",
+        "pdf": "docs/equipamentos/IEF01.pdf",
+        "link": ""
+      }
+    ]
+  },
+  {
+    "id": "FGA",
+    "nome": "FGA",
+    "categoria": "CX_PN",
+    "label": "CX e PN",
+    "planta": "docs/plantas/FGA.pdf",
+    "itens": [
+      {
+        "tipo": "CX",
+        "nome": "CX01",
+        "codigo": "FGA-CX01",
+        "km": "",
+        "pdf": "docs/equipamentos/FGA-CX01.pdf",
+        "link": ""
+      },
+      {
+        "tipo": "PN",
+        "nome": "PN02",
+        "codigo": "FGA-PN02",
+        "km": "",
+        "pdf": "docs/equipamentos/FGA-PN02.pdf",
+        "link": ""
+      }
+    ]
+  },
+  {
+    "id": "FCE",
+    "nome": "FCE",
+    "categoria": "CX",
+    "label": "CX",
+    "planta": "docs/plantas/FCE.pdf",
+    "itens": [
+      {
+        "tipo": "CX",
+        "nome": "CX01",
+        "codigo": "FCE-CX01",
+        "km": "",
+        "pdf": "docs/equipamentos/FCE-CX01.pdf",
+        "link": ""
+      },
+      {
+        "tipo": "CX",
+        "nome": "CX02",
+        "codigo": "FCE-CX02",
+        "km": "",
+        "pdf": "docs/equipamentos/FCE-CX02.pdf",
+        "link": ""
+      }
+    ]
+  }
+];
