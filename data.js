869405a256e521b25060e3b40469b2538140c1c5
MatheusPrado0096 <matheusdprado179@gmail.com>
2026-09-16 11:19:16 -0300

    Update link to new Google Maps URL FSJ ABR 10

--- data.js
+++ data.js
@@ -151,7 +151,7 @@ window.PATIOS = [
         "codigo": "FSJ02",
         "km": "",
         "pdf": "docs/equipamentos/FSJ2_ABR10.pdf",
-        "link": "https://share.google/qtDVyaCvPL9WmGeMN"
+        "link": "https://maps.app.goo.gl/yzYdSUYbGVsyeM4j8"
       },
       {
         "tipo": "Abrigo",
